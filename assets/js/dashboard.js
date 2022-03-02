b7fccce092a3d2f13ab094ac14976a9c26492a47
Georgi Dobrishinov <dobrishinov@gmail.com>
2022-03-02 18:14:38 +0200

    Load values in the dashboard with Ajax

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,4 +3,34 @@
 
   feather.replace({ 'aria-hidden': 'true' })
 
-})()
+})(),
+
+
+$(document).ready(function () {
+  widget.getDataFromService();
+});
+
+
+setInterval(function () {
+  widget.getDataFromService();
+}, 15 * 1000);
+
+
+var widget = function() {
+  var callService = function() {
+    $.ajax({
+      url: "https://thingspeak.com/channels/1663924/feed/last.json?results=1",
+      type: "GET",
+      dataType: "json",
+      success: function (response) {
+        // console.log(response);
+        $('#uSv').html(response.field3);
+        $('#CPM').html(response.field2);
+        $('#CPS').html(response.field1);
+      }
+    });
+  }
+  return {
+    getDataFromService: function() { callService(); }
+  };
+}();
